d8708e15dbe6b51f380dd818b10309273f41e503
525UmerYousaf <umer.yousaf.587>
2023-05-21 17:29:36 +0500

    "I am working on Posts layout"

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:facebook_ui/widgets/profile_avatar.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first, avoid_print
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
+import 'package:facebook_ui/widgets/profile_avatar.dart';
 
 import '../models/post_model.dart';
 
@@ -18,11 +20,33 @@ class PostContainer extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 8),
       color: Colors.white,
       child: Column(
-        //  With following property Column is stretched to
-        //  all the width of the Container.
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          _PostHeader(post: post),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Column(
+              //  With following property Column is stretched to
+              //  all the width of the Container.
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _PostHeader(post: post),
+                const SizedBox(height: 4),
+                Text(post.caption),
+                post.imageUrl != ''
+                    ? const SizedBox.shrink()
+                    : const SizedBox(height: 6),
+              ],
+            ),
+          ),
+          post.imageUrl != ''
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: CachedNetworkImage(imageUrl: post.imageUrl),
+                )
+              : const SizedBox.shrink(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: _PostStats(post: post),
+          ),
         ],
       ),
     );
@@ -42,13 +66,54 @@ class _PostHeader extends StatelessWidget {
       children: [
         ProfileAvatar(imageUrl: post.user.imageUrl),
         const SizedBox(width: 8),
-        Column(
+        Expanded(
+          //  With wraping Column inside Expanded widget the Column takes all
+          //  available space (width).
+          child: Column(
+            //  With following property I'm forcing user profile info
+            //  to start from left-most.
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                post.user.name,
+                style: const TextStyle(fontWeight: FontWeight.w600),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '${post.timeAgo} ∙',
+                    style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                  ),
+                  Icon(Icons.public, color: Colors.grey[600], size: 12),
+                ],
+              ),
+            ],
+          ),
+        ),
+        IconButton(
+          onPressed: () => print('More Button pressed'),
+          icon: const Icon(Icons.more_horiz),
+        ),
+      ],
+    );
+  }
+}
+
+class _PostStats extends StatelessWidget {
+  final Post post;
+  const _PostStats({
+    Key? key,
+    required this.post,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
           children: [
-            Text(post.user.name),
-            Row(
-              children: [
-                Text('${post.timeAgo}'),
-              ],
+            Container(
+              decoration: BoxDecoration(),
             ),
           ],
         ),
